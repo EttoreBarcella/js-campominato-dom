6b75476c8d6aaa825dc28157272fd60cac5bd0ff
EttoreBarcella <ettore.barcella@gmail.com>
2022-05-10 11:54:03 +0200

    ask user prompt+fizzbuzz scheme added in html css

--- main.js
+++ main.js
@@ -35,3 +35,17 @@ function wichDiff(diff) {
         return triesNumber = 50
     }
 }
+// --- MAIN ---
+const result = document.getElementById('result')
+let difficulty
+
+do {
+    difficulty = Number(prompt(`Scegli un numero  tra 0 e 2`))
+} while (isNaN(difficulty) || difficulty < 0 || difficulty > 2)
+
+let maxDifficultyRange = wichDiff(difficulty)
+mineField(maxDifficultyRange, '.field')
+
+const bombs = bombGenerator(16, maxDifficultyRange)
+console.log(bombs)
+
